8184953c679bf2f01f53c001df7a6bb13a3ee890
Fraank98 <danilo806@icloud.com>
2022-05-10 22:52:41 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image, Pressable } from 'react-native';
+import { Camera } from 'expo-camera';
+
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [imageUri, setImageUri] = useState(false);
+  const [camera, setCamera] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (!camera) return
+      const photo = await camera.takePictureAsync(null)
+      setImageUri(photo.uri)
+  }
+
+  function closeButton() {
+    setImageUri(false)
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={(ref) => setCamera(ref)}>
+        <View style={styles.buttonContainer}>
+          {imageUri ? null : <TouchableOpacity
+            style={styles.buttonSnap}
+            onPress={takePicture}>
+            <Text style={styles.text}> Snap </Text>
+          </TouchableOpacity>
+          }
+        </View>
+      </Camera>
+      {imageUri ? <Image source={{ uri: imageUri }} style={styles.preview} /> : null}
+      {imageUri ? (
+        <View style={styles.bottom}>
+          <TouchableOpacity style={styles.buttonClose} onPress={closeButton}>
+            <Text style={styles.text}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
@@ -13,8 +60,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonSnap: {
+    position: 'absolute',
+    bottom: 60,
+    backgroundColor: 'white'
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: 'red',
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+
+  buttonClose: {
+    position: 'absolute',
+    bottom: 60,
+    backgroundColor: 'white',
+    paddingRight: 5,
+    paddingLeft: 5
+  },
+
+  preview: {
+    height: '100%', 
+  },
+
 });
