2da9a02f09c75e06e85461c71ba3408bc358b3aa
Fraank98 <danilo806@icloud.com>
2022-05-22 17:17:03 +0200

    Flash and vary improvments

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button, Image, Pressable } from 'react-native';
-import { Camera } from 'expo-camera';
 
+import { Camera } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [imageUri, setImageUri] = useState(false);
   const [camera, setCamera] = useState(null);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [activateFlash, setActivateFlash] = useState(false);
   const type = Camera.Constants.Type.back;
+  const flashIcon = useRef(null)
+  const initialflashText = {
+    text: 'Off',
+    style: {
+      color: 'white',
+    },
+    iconColor: {
+      color:'white',
+    }
+  }
+  const [flashText, setFlashText] = useState(initialflashText)
 
   useEffect(() => {
     (async () => {
@@ -17,14 +31,40 @@ export default function App() {
   }, []);
 
   const takePicture = async () => {
-    if (!camera) return
-      const photo = await camera.takePictureAsync(null)
-      setImageUri(photo.uri)
-  }
+    if (activateFlash) {
+      setFlash(Camera.Constants.FlashMode.torch);
+      console.log("sono nell' IF")
+    }
+    if (!camera) return;
+    const photo = await camera.takePictureAsync(null);
+    setImageUri(photo.uri);
+    setFlash(Camera.Constants.FlashMode.off);
+    setFlashText(initialflashText)
+    setActivateFlash(false)
+  };
 
-  function closeButton() {
-    setImageUri(false)
-  }
+  const toggleFlash = () => {
+    if (!activateFlash){
+      setActivateFlash(true);
+      setFlashText({
+        text: 'On',
+        style: {
+          color: 'yellow',
+        },
+        iconColor: {
+          color:'yellow',
+        }
+      })
+    }
+    else {
+      setActivateFlash(false)
+      setFlashText(initialflashText)
+    }
+  };
+
+  const closeButton = () => {
+    setImageUri(false);
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -35,24 +75,30 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={(ref) => setCamera(ref)}>
+      {!imageUri && 
+      <Camera flashMode={flash} style={styles.camera} type={type} ref={(ref) => setCamera(ref)}>
         <View style={styles.buttonContainer}>
-          {imageUri ? null : <TouchableOpacity
-            style={styles.buttonSnap}
+          {!imageUri && <TouchableOpacity
+            style={styles.snapButton}
             onPress={takePicture}>
-            <Text style={styles.text}> Snap </Text>
-          </TouchableOpacity>
+            <Ionicons name="camera" size={60} color="red"/>
+          </TouchableOpacity> 
           }
         </View>
-      </Camera>
-      {imageUri ? <Image source={{ uri: imageUri }} style={styles.preview} /> : null}
-      {imageUri ? (
+        <Pressable onPress={toggleFlash} style={styles.flashButton}>
+          <Ionicons name="flash" size={40} ref={flashIcon} style={flashText.iconColor} />
+          <Text style={[flashText.style, styles.flashText]}>{flashText.text}</Text>
+        </Pressable>
+      </Camera>}
+      {!!imageUri && <Image source={{ uri: imageUri }} style={styles.preview} />}
+      {!!imageUri &&
         <View style={styles.bottom}>
-          <TouchableOpacity style={styles.buttonClose} onPress={closeButton}>
-            <Text style={styles.text}>Close</Text>
+          <TouchableOpacity style={styles.closeButton} onPress={closeButton}>
+            <Ionicons name="arrow-back-circle" size={60} color="red"/>
+            {/* <Text style={styles.backTest}>back</Text> */}
           </TouchableOpacity>
         </View>
-      ) : null}
+      }
     </View>
   );
 }
@@ -62,34 +108,50 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
-    flex: 1,
+    // flex: 1,
+    height: '100%', 
   },
+
   buttonContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonSnap: {
+
+  snapButton: {
     position: 'absolute',
     bottom: 60,
-    backgroundColor: 'white'
   },
+
+  flashButton: {
+    position: 'absolute',
+    top: 50,
+    right: 15,
+  },
+
+  flashText: {
+    fontSize: 10,
+    position: 'absolute',
+    marginTop : 40,
+    marginLeft : 20,
+  },
+
   text: {
     fontSize: 30,
     fontWeight: "bold",
     color: 'red',
   },
+  
   bottom: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
 
-  buttonClose: {
+  closeButton: {
     position: 'absolute',
     bottom: 60,
-    backgroundColor: 'white',
     paddingRight: 5,
     paddingLeft: 5
   },
@@ -97,5 +159,13 @@ const styles = StyleSheet.create({
   preview: {
     height: '100%', 
   },
+
+  backTest: {
+    position: 'absolute',
+    marginTop: 60,
+    marginLeft: 14,
+    color: 'red',
+    fontSize: 18,
+  }
 
 });
